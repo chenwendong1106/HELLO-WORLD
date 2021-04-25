1fb92a25ae7f8cffa66bac493b2d9c9881d06082
chenwendong <chenwendong>
2021-04-25 14:50:52 +0800

    新建测试类

---
+++ src/main/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.test;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author chenwendong
+ * @date 2021/4/25
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
