f62cf1223e876233fc446cb9550ba859c4e93b3e
chenwendong <chenwendong>
2021-04-28 11:36:34 +0800

    test

--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -7,6 +7,6 @@ package main.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world!-----------------");
     }
 }
